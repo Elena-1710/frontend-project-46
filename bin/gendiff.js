7b801adc18225becd798482b2e9d65c2aba78d4b
Elena-1710 <zhunina.lena@yandex.ru>
2025-01-31 05:24:16 +0300

    modified gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ const program = new Command();
 program 
 .name('gendiff')
 .version ('1.0.0')
+.helpOption ('-h, --help', 'output usage information')
 .description ('Compares two configuration files and shows a difference.')
 .usage('[options] <filepath1> <filepath2>');
 
