ea19f5672d36bfa6c4583f582bd3ed4029da3657
Elena-1710 <zhunina.lena@yandex.ru>
2025-01-31 16:10:20 +0300

    eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
-import {Command} from 'commander';
+import { Command } from 'commander';
 
 const program = new Command();
 
-program 
-.name('gendiff')
-.version ('1.0.0')
-.helpOption ('-h, --help', 'output usage information')
-.description ('Compares two configuration files and shows a difference.')
-.option('-f, --format [type]', 'output format (options: stylish, plain, json)', 'stylish')
-.arguments('<filepath1> <filepath2>')
+program
+  .name('gendiff')
+  .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format (options: stylish, plain, json)', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = gendiff(filepath1, filepath2, program.opts().format);
+    console.log(diff);
+  });
 
 program.parse();
