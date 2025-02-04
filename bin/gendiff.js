2c3e0488c51032004d700773e6a52526030da2f4
Elena-1710 <zhunina.lena@yandex.ru>
2025-02-04 03:45:11 +0300

    mod gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { program } from 'commander';
 import genDiff from '../src/index.js';
 
+const program = new Command();
 program
   .name('gendiff')
   .version('1.0.0')
